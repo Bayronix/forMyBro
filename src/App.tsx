6f3cfe635cf5cd19f07b2f57e310a5b4183d7391
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-12-10 18:33:34 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import './App.css';
+import OIPImage from '../public/OIP.jpg';
+import MaxResImage from '../public/maxresdefault.jpg';
 
 function App() {
   return (
     <>
       <h1 className="SlavaUkrain">Слава Україні!</h1>
-      <img className="dinax" src="../public/OIP.jpg" alt="Пейзаж України" />
+      <img className="dinax" src={OIPImage} alt="Пейзаж України" />
       <p className="read-the-docs">
         Геро<><span className="read-the-docs-span">ям Слава!</span></>
       </p>
 
-      <img className="dinax" src="../public/maxresdefault.jpg" alt="Тризуб" />
+      <img className="dinax" src={MaxResImage} alt="Тризуб" />
       <a href="https://ab3.army/" className="link-button" target="_blank" rel="noopener noreferrer">
         Долучись до 3 штурмової 
       </a>
-      
-
-      
     </>
   );
 }
