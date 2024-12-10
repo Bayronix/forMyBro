c9a4549d5fe9d50b15ef5e7d274100b7c7b21abc
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-12-10 18:28:45 +0100

    Потужний комммііттттттттттттттттттттттттттттттттттт

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="SlavaUkrain">Слава Україні!</h1>
+      <img className="dinax" src="../public/OIP.jpg" alt="Пейзаж України" />
+      <p className="read-the-docs">
+        Геро<><span className="read-the-docs-span">ям Слава!</span></>
+      </p>
+
+      <img className="dinax" src="../public/maxresdefault.jpg" alt="Тризуб" />
+      <a href="https://ab3.army/" className="link-button" target="_blank" rel="noopener noreferrer">
+        Долучись до 3 штурмової 
+      </a>
+      
+
+      
+    </>
+  );
+}
+
+export default App;
